e8eeae3049344ee8dc7c26307dbf9330da103681
Boris Egorov <egorov@linux.com>
2016-10-29 00:06:13 +0700

    Use JSON instead of text for storing movie data

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,39 +1,34 @@
 #!/usr/bin/env python3
 
 import fresh_tomatoes
-import logging
 import media
+import json
+import logging
 
 
-MOVIES_FILE = "movies.txt"
+MOVIES_FILE = "movies.json"
 
 
 def load_movies_data(filename):
-    """Loads movie details from specified @filename"""
+    """Loads movie details from specified @filename in JSON format"""
     movies = []
     with open(filename) as movies_file:
-        while True:
+        try:
+            data = json.load(movies_file)
+        except json.decoder.JSONDecodeError:
+            logging.critical("Cannot get movies data.")
+            return movies
+
+        for movie_data in data:
             try:
-                title = movies_file.readline()
-                duration = int(movies_file.readline())
-                storyline = movies_file.readline()
-                poster_url = movies_file.readline()
-                trailer_url = movies_file.readline()
-                movie = media.Movie(title, duration, storyline,
-                                    poster_url, trailer_url)
-            except ValueError:
-                logging.warning("Cannot load a movie; skipping rest of input")
-                # Following reads is dangerous, stop now
-                break
+                movies.append(media.Movie(**movie_data))
+            except TypeError:
+                logging.warning("Cannot load a movie; skipping an item")
 
-            movies.append(movie)
-
-            if not movies_file.readline():
-                # Reached end of file, no more data
-                break
     return movies
 
 if __name__ == "__main__":
     movies = load_movies_data(MOVIES_FILE)
-    movies.sort(key=lambda m: m.title)
-    fresh_tomatoes.open_movies_page(movies)
+    if movies:
+        movies.sort(key=lambda m: m.title)
+        fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -4,12 +4,12 @@ import webbrowser
 class Movie:
     """This class provides a way to store movie related information"""
 
-    def __init__(self, title, duration, storyline, poster_image,
+    def __init__(self, title, duration, storyline, poster_image_url,
                  trailer_youtube_url):
         self.title = title
         self.duration = duration
         self.storyline = storyline
-        self.poster_image_url = poster_image
+        self.poster_image_url = poster_image_url
         self.trailer_youtube_url = trailer_youtube_url
 
     def show_trailer(self):
